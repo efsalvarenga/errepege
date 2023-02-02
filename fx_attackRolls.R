1bf406bc32419b88f25f95fd6901dde4cd6686ea
efsalvarenga <efsa@bath.edu>
2023-02-02 23:36:58 +0100

    Update fx_attackRolls.R

--- fx_attackRolls.R
+++ fx_attackRolls.R
@@ -1,28 +1,28 @@
 library(dplyr)
 
 # defining function
-fx_attackRolls <- function(pcs, npcs, seed = NULL,
+fx_attackRolls <- function(atck, dfnd, seed = NULL,
                            output = c('detail', 'summary', 'war')) {
   
   if (!is.null(seed)) { set.seed(seed) }
   
   # attack roll
-  atckRolls <- lapply(names(npcs), function (name) {
+  atckRolls <- lapply(names(atck), function (name) {
     # attack rolls and choice of targets (all random)
-    roll = sample(1:20, npcs[[name]]$number, replace = T)
+    roll = sample(1:20, atck[[name]]$number, replace = T)
     criticals <- roll == 20
-    roll = roll * npcs[[name]]$condition + npcs[[name]]$toHit
-    tgts = sample(names(pcs), npcs[[name]]$number, replace = T)
+    roll = roll * atck[[name]]$condition + atck[[name]]$toHit
+    tgts = sample(names(dfnd), atck[[name]]$number, replace = T)
     
     # resolving rolls
-    tgts_ac <- sapply(tgts, function (tgt) { pcs[[tgt]]$ac })
+    tgts_ac <- sapply(tgts, function (tgt) { dfnd[[tgt]]$ac })
     outcome = roll > tgts_ac
     outcome[criticals] <- TRUE
     
     # calculating damage
     dmgs <- outcome * 1
     dmgs[criticals] <- 1.5
-    dmgs <- sample(npcs[[name]]$dmg, npcs[[name]]$number, replace = T) * dmgs
+    dmgs <- sample(atck[[name]]$dmg, atck[[name]]$number, replace = T) * dmgs
     dmgs <- round(dmgs)
     
     # preparing outcome data
@@ -34,13 +34,13 @@ fx_attackRolls <- function(pcs, npcs, seed = NULL,
                       hit = hit))
   })
   # improve naming
-  names(atckRolls) <- names(npcs)
+  names(atckRolls) <- names(atck)
   
   if (output == 'detail') {
     return (atckRolls)
   } else {
     # summarised results
-    atckRollsS <- lapply(names(npcs), function (name) {
+    atckRollsS <- lapply(names(atck), function (name) {
       # skiping empty tables
       if (nrow(atckRolls[[name]]) == 0) { return (NA) }
       
@@ -53,13 +53,13 @@ fx_attackRolls <- function(pcs, npcs, seed = NULL,
       return (atckSumm)
     })
     # improve naming
-    names(atckRollsS) <- names(npcs)
+    names(atckRollsS) <- names(atck)
     
     if (output == 'summary') {
       return (atckRollsS)
     } else {
       # war summary
-      atckRollsW <- lapply(names(npcs), function (name) {
+      atckRollsW <- lapply(names(atck), function (name) {
         # skiping empty tables
         if (nrow(atckRollsS[[name]]) == 0) {
           return (NA)
@@ -75,7 +75,7 @@ fx_attackRolls <- function(pcs, npcs, seed = NULL,
         return (atckSumm)
       })
       # improve naming
-      names(atckRollsW) <- names(npcs)
+      names(atckRollsW) <- names(atck)
       
       return (atckRollsW)
     }
@@ -90,13 +90,6 @@ fx_attackRolls <- function(pcs, npcs, seed = NULL,
 pcs_tgt <- list(
   ggkeGhuse = list(ac = 19),
   ggkeRest = list(ac = 18)
-  # c = list(ac = 16),
-  # g = list(ac = 14),
-  # b = list(ac = 10),
-  # t = list(ac = 13),
-  # m = list(ac = 12),
-  # d1 = list(ac = 14),
-  # d2 = list(ac = 15)
 )
 
 pcs_atck <- list(
@@ -112,6 +105,8 @@ pcs_atck <- list(
 
 npcs_tgt <- list(
   skelzombie = list(ac = 11)
+  # skullComm = list(ac = 18),
+  # lizardUndead = list(ac = 14)
 )
 
 # npcs side set-up
@@ -119,24 +114,24 @@ npcs_tgt <- list(
 # .: condition: a = 1.3; n = 1; d = 0.7
 npcs_atck <- list(
   skelzombie = list(toHit = 3,
-                    dmg = c(3:8),
-                    number = 480,
-                    condition = 1)
-  # skeleton = list(toHit = 4,
-  #                 dmg = c(3:8),
-  #                 number = 0,
-  #                 condition = 1),
-  # zombie = list(toHit = 3,
-  #               dmg = c(2:7),
-  #               number = 0,
-  #               condition = 1)
+                    dmg = c(5:10), # c(3:8) when commander dies
+                    number = 550,
+                    condition = 1.125), # 1 when commander dies
+  skullComm = list(toHit = 7,
+                   dmg = c(6:18),
+                   number = 2,
+                   condition = 1),
+  lizardUndead = list(toHit = 7,
+                      dmg = c(7:21), # c(7:21) when commander dies
+                      number = 2,
+                      condition = 1.125) # 1 when commander dies
 )
 
 # pcs attack roll
-fx_attackRolls(pcs = npcs_tgt, npcs = pcs_atck, seed = NULL, output = 'war')
+fx_attackRolls(atck = pcs_atck, dfnd = npcs_tgt, seed = NULL, output = 'war')
 
 # npcs attack roll
-fx_attackRolls(pcs = pcs_tgt, npcs = npcs_atck, seed = 1, output = 'war')
+fx_attackRolls(atck = npcs_atck, dfnd = pcs_tgt, seed = NULL, output = 'war')
 
 
 
@@ -168,4 +163,4 @@ npcs <- list(
                 condition = 1)
 )
 
-fx_attackRolls(pcs = pcs, npcs = npcs, seed = NULL, output = 'summary')
+fx_attackRolls(atck = npcs, dfnd = pcs, seed = NULL, output = 'summary')
