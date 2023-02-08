ea183783d8b210ef3f14a377943dad8a52008505
efsalvarenga <efsa@bath.edu>
2023-02-08 15:40:04 +0100

    prepared for battle on saturday

--- combat/scratchRoundsSim.R
+++ combat/scratchRoundsSim.R
@@ -11,12 +11,68 @@ source('./combat/fx_attackRolls.R')
 #                               SCRATCH ROUNDS                                #
 # =========================================================================== #
 
+# SMALL COMBAT
 # load JSON data
 # condition: E(d20D) = 7.2 (0.7x); E(d20) = 10.5 (1x); E(d20A) = 13.8 (1.3x)
-npcsDf <- fx_convListDF(rjson::fromJSON(
+npcsDf1 <- fx_convListDF(rjson::fromJSON(
   file = './combat/Glasrath_AssautTgt.json'))
-pcsDf <- fx_convListDF(rjson::fromJSON(
+pcsDf1 <- fx_convListDF(rjson::fromJSON(
   file = './combat/Glasrath_AssautPCs.json'))
 
-fx_attackRolls(atck = npcsDf, dfnd = pcsDf, seed = NULL,
+fx_attackRolls(atck = npcsDf1, dfnd = pcsDf1, seed = NULL,
                maxAtckMult = 6, output = 'summary')
+
+
+# LARGE COMBAT
+# load JSON data
+# condition: E(d20D) = 7.2 (0.7x); E(d20) = 10.5 (1x); E(d20A) = 13.8 (1.3x)
+pcsList <- c(
+  # 'wtht',
+  # 'ggke',
+  'gmnz',
+  # 'tfdd',
+  'ngn'
+  # 'dmth'
+)
+
+npcsList <- c(
+  # 't',
+  'g'
+  # 'c'
+)
+
+pcsDf2 <- lapply(pcsList, function(x) {
+  fx_convListDF(rjson::fromJSON(
+    file = paste0('./combat/Glasrath_Alliance_', x, '.json')))
+}) %>% bind_rows() %>%
+  filter(number > 0)
+
+
+npcsDf2 <- lapply(npcsList, function(x) {
+  fx_convListDF(rjson::fromJSON(
+    file = paste0('./combat/Glasrath_Uchbur_', x, '.json')))
+}) %>% bind_rows() %>%
+  filter(number > 0)
+
+# npcs attacking
+npcsRound <- left_join(pcsDf2,
+          fx_attackRolls(atck = npcsDf2, dfnd = pcsDf2, seed = NULL,
+               maxAtckMult = 3, output = 'warsummary'),
+          by = c('name' = 'tgt')) %>%
+  mutate(deaths = round(total / hp, digits = 1),
+         survivors = number - deaths,
+         resistance = round(survivors / number, digits = 2))
+npcsRound
+
+# pcs attacking
+pcsRound <- left_join(npcsDf2,
+          fx_attackRolls(atck = pcsDf2, dfnd = npcsDf2, seed = NULL,
+                         maxAtckMult = 3, output = 'warsummary'),
+          by = c('name' = 'tgt')) %>%
+  mutate(total = ifelse(is.na(total), 0, total),
+         deaths = round(total / hp, digits = 1),
+         survivors = number - deaths,
+         resistance = round(survivors / number, digits = 2))
+pcsRound
+
+
