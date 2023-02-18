b19e46b74a6a0dfe8352faf59b32685774822b34
efsalvarenga <efsa@bath.edu>
2023-02-18 18:10:16 +0100

    making it a function

--- political/glasrathAllianceFactions.R
+++ political/glasrathAllianceFactions.R
@@ -8,36 +8,53 @@ library(rjson)
 source('./combat/fx_attackRolls.R')
 
 # =========================================================================== #
-#                                    ROUNDS                                   #
+#                                  FUNCTION                                   #
 # =========================================================================== #
-
-alliance <- fx_convListDF(rjson::fromJSON(
-  file = './political/glasrathAllianceFactions.json'))
-alliance
-
-stopCond <- 270
-rounds <- 0
-
-while (rounds < stopCond) {
-  alliance <- alliance %>%
-    mutate(roll = sample(1:20, n()),
-           result = roll + 10 * tier + expansion,
-           devAdd = round(result / max(result), 1),
-           turfAdd = ifelse(dev + devAdd + turf > 12, turf/sum(turf), 0),
-           turfNew = ifelse(turfAdd + turf < 1, 0, turfAdd + turf),
-           turfNrm = round(turfNew * 12 / sum(turfNew), 1),
-           devNew = ifelse(turfAdd > 0, 0, round(dev + devAdd, 1)))
+factionGame <- function (factionsDF, nRounds, seed = NULL) {
+  # set seed if requested by argument
+  if (!is.null(seed)) { set.seed(seed) }
   
-  alliance <- alliance %>%
-    select(name,
-           tier,
-           type,
-           turf = turfNrm,
-           dev = devNew,
-           expansion)
+  # start rounds counter
+  rounds <- 0
   
-  rounds <- rounds + 1
+  while (rounds < nRounds) {
+    # calculate round gains in development and turf
+    factionsDF <- factionsDF %>%
+      mutate(roll = sample(1:20, n()),
+             result = roll + 10 * tier + tenacity,
+             devAdd = round(result / max(result), 1),
+             turfAdd = ifelse(dev + devAdd + turf > 12, turf/sum(turf), 0),
+             turfNew = ifelse(turfAdd + turf < 1, 0, turfAdd + turf),
+             turfNrm = round(turfNew * 12 / sum(turfNew), 1),
+             devNew = ifelse(turfAdd > 0, 0, round(dev + devAdd, 1)))
+    
+    # create updated data frame for factions
+    factionsDF <- factionsDF %>%
+      select(name,
+             type,
+             tier,
+             hold,
+             turf = turfNrm,
+             dev = devNew,
+             tenacity)
+    
+    # update rounds counter
+    rounds <- rounds + 1
+  }
+  
+  return (factionsDF)
 }
 
-alliance
+# =========================================================================== #
+#                             Glasrath Alliance                               #
+# =========================================================================== #
+alliance <- fx_convListDF(rjson::fromJSON(
+  file = './political/glasrathAllianceFactions.json'))
+
+# resolving the first 2 months while characters are preparing to leave
+# to the ammuinoth
+alliance <- factionGame(alliance, nRounds = 60, seed = 1)
+alliance %>%
+  mutate(turf = round(turf),
+         dev = round(dev))
 
